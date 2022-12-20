6b99467bac8e3b73babcc7a91eba4ced3c24e637
Peter Oseland <peter@checkin.no>
2022-12-20 11:11:43 +0100

    Remove unnecessary cleanup of scripts and links

--- build/main.js
+++ build/main.js
@@ -5,30 +5,9 @@ import {getEventRegistrationForm as $jfIsG$getEventRegistrationForm} from "@chec
 
 
 
-var $03e110a58a2ad238$export$2e2bcd8739ae039 = ()=>{
-    const headScripts = document.head.getElementsByTagName("script");
-    const headLinks = document.head.getElementsByTagName("link");
-    const scripts = [];
-    const links = [];
-    for (const script of headScripts)if (script.src.includes("registration.checkin.no")) scripts.push(script);
-    for (const link of headLinks)if (link.href.includes("registration.checkin.no")) links.push(link);
-    scripts.forEach((script)=>{
-        document.head.removeChild(script);
-    });
-    links.forEach((link)=>{
-        document.head.removeChild(link);
-    });
-};
-
-
 const $3adeb65f111af5a9$var$EventRegistrationForm = ({ eventId: eventId  })=>{
     const eventRegistrationForm = (0, $jfIsG$useMemo)(()=>{
         return (0, $jfIsG$getEventRegistrationForm)(eventId);
-    }, []);
-    (0, $jfIsG$useEffect)(()=>{
-        return ()=>{
-            (0, $03e110a58a2ad238$export$2e2bcd8739ae039)();
-        };
     }, []);
     (0, $jfIsG$useEffect)(()=>{
         document.event_id = eventId;
--- src/EventRegistrationForm.tsx
+++ src/EventRegistrationForm.tsx
@@ -1,17 +1,10 @@
 import React, { useEffect, useMemo } from 'react';
 import { getEventRegistrationForm } from '@checkin.no/event-registration';
-import cleanupScripts from './utils/cleanupScripts';
 
 const EventRegistrationForm = ({eventId}) => {
     const eventRegistrationForm = useMemo(() => {
         return getEventRegistrationForm(eventId);
     }, []);
-
-    useEffect(() => {
-        return () => {
-            cleanupScripts();
-        }
-    }, [])
 
     useEffect(() => {
         document.event_id = eventId;
