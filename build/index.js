00f42b7725cbb6ded27cdcf5981439552c697eed
Peter Oseland <peter@checkin.no>
2023-02-20 16:21:26 +0100

    Update event registration form dependency

--- build/index.js
+++ build/index.js
@@ -1,38 +1,29 @@
-var $dnHPu$reactjsxruntime = require("react/jsx-runtime");
-var $dnHPu$react = require("react");
-var $dnHPu$checkinnoeventregistration = require("@checkin.no/event-registration");
-
-function $parcel$defineInteropFlag(a) {
-  Object.defineProperty(a, '__esModule', {value: true, configurable: true});
-}
-function $parcel$export(e, n, v, s) {
-  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
-}
-
-$parcel$defineInteropFlag(module.exports);
-
-$parcel$export(module.exports, "default", () => $f5bfd4ce37214f4f$export$2e2bcd8739ae039);
+import {jsx as $jfIsG$jsx} from "react/jsx-runtime";
+import {useMemo as $jfIsG$useMemo, useEffect as $jfIsG$useEffect} from "react";
+import {getEventRegistrationForm as $jfIsG$getEventRegistrationForm} from "@checkin.no/event-registration";
 
 
 
-const $91d1d5d8a0528c9f$var$EventRegistrationForm = ({ eventId: eventId  })=>{
-    const eventRegistrationForm = (0, $dnHPu$react.useMemo)(()=>{
-        return (0, $dnHPu$checkinnoeventregistration.getEventRegistrationForm)(eventId);
+
+const $3adeb65f111af5a9$var$EventRegistrationForm = ({ eventId: eventId  })=>{
+    const eventRegistrationForm = (0, $jfIsG$useMemo)(()=>{
+        return (0, $jfIsG$getEventRegistrationForm)(eventId);
     }, []);
-    (0, $dnHPu$react.useEffect)(()=>{
+    (0, $jfIsG$useEffect)(()=>{
         document.event_id = eventId;
         if (document.event_id === eventId) eventRegistrationForm.initRegistrationForm();
         else eventRegistrationForm.changeEvent(eventId);
     }, [
         eventId
     ]);
-    return /*#__PURE__*/ (0, $dnHPu$reactjsxruntime.jsx)("div", {
+    return /*#__PURE__*/ (0, $jfIsG$jsx)("div", {
         id: "checkin_registration"
     });
 };
-var $91d1d5d8a0528c9f$export$2e2bcd8739ae039 = $91d1d5d8a0528c9f$var$EventRegistrationForm;
+var $3adeb65f111af5a9$export$2e2bcd8739ae039 = $3adeb65f111af5a9$var$EventRegistrationForm;
 
 
-var $f5bfd4ce37214f4f$export$2e2bcd8739ae039 = (0, $91d1d5d8a0528c9f$export$2e2bcd8739ae039);
+var $d415641d0cfd8c85$export$2e2bcd8739ae039 = (0, $3adeb65f111af5a9$export$2e2bcd8739ae039);
 
 
+export {$d415641d0cfd8c85$export$2e2bcd8739ae039 as default};
